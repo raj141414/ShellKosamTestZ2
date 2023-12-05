6b85e0ee92c5e06da333426ab67d885dd6c653fd
raj141414 <148778305+raj141414@users.noreply.github.com>
2023-12-05 09:54:07 +0530

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -141,7 +141,7 @@ def get_readable_message():
             msg += f"\n<b>File Name</b> » <i>{escape(f'{download.name()}')}</i>\n\n"
         msg += f"⌑ <b>{download.status()}</b>"
         if download.status() not in [MirrorStatus.STATUS_SEEDING, MirrorStatus.STATUS_PAUSED,
-                                     MirrorStatus.STATUS_QUEUEDL, MirrorStatus.STATUS_QUEUEUP]:
+                                     MirrorStatus.STATUS_QUEUEDL, MirrorStatus.STATUS_QUEUEUP, MirrorStatus.STATUS_LOCAL]:
             msg += f" » {download.speed()}"
             msg += f"\n⌑ {get_progress_bar_string(download.progress())} » {download.progress()}"
             msg += f"\n⌑ <code>Done   </code>: {download.processed_bytes()} of {download.size()}"
