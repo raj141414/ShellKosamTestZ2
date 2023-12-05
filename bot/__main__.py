8d60404afb571393562a63c7a457a011a26040fd
raj141414 <148778305+raj141414@users.noreply.github.com>
2023-12-05 17:20:13 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -62,7 +62,7 @@ async def stats(_, message, edit_mode=False):
     mem_p       = memory.percent
     swap        = swap_memory()
 
-    bot_stats = f'<b><i><u>Zee Bot Statistics</u></i></b>\n\n'\
+    bot_stats = f'<b><i><u>Blaster Bot Statistics</u></i></b>\n\n'\
                 f'<code>CPU  : {get_progress_bar_string(cpuUsage)}</code> {cpuUsage}%\n' \
                 f'<code>RAM  : {get_progress_bar_string(mem_p)}</code> {mem_p}%\n' \
                 f'<code>SWAP : {get_progress_bar_string(swap.percent)}</code> {swap.percent}%\n' \
@@ -73,7 +73,7 @@ async def stats(_, message, edit_mode=False):
                 f'<code>Downloaded      : </code> {recv}\n' \
                 f'<code>Total Bandwidth : </code> {tb}'
 
-    sys_stats = f'<b><i><u>Zee System Statistics</u></i></b>\n\n'\
+    sys_stats = f'<b><i><u>Blaster System Statistics</u></i></b>\n\n'\
                 f'<b>System Uptime:</b> <code>{sysTime}</code>\n' \
                 f'<b>CPU:</b> {get_progress_bar_string(cpuUsage)}<code> {cpuUsage}%</code>\n' \
                 f'<b>CPU Total Core(s):</b> <code>{cpu_count(logical=True)}</code>\n' \
@@ -164,7 +164,7 @@ async def send_repo_stats(_, query):
             if version != vtag:
                 update_info =  f'⚠️ New Version Update Available ⚠️'
 
-    repo_stats = f'<b><i><u>Zee Repository Info</u></i></b> \n\n' \
+    repo_stats = f'<b><i><u>Blaster Repository Info</u></i></b> \n\n' \
                  f'<b><i>Official Repository</i></b>        \n'   \
                  f'<code>- Updated   : </code> {commit_date}\n'   \
                  f'<code>- Version   : </code> {vtag}       \n'   \
@@ -254,10 +254,10 @@ async def start(_, message):
     elif config_dict['DM_MODE'] and message.chat.type != message.chat.type.SUPERGROUP:
         start_string = 'Bot Started.\n' \
                        'Now I will send all of your stuffs here.\n' \
-                       'Use me at: @Z_Mirror'
+                       'U Can See Me At: @Blaster_Originals'
     elif not config_dict['DM_MODE'] and message.chat.type != message.chat.type.SUPERGROUP:
         start_string = 'Sorry, you cannot use me here!\n' \
-                       'Join: @Z_Mirror to use me.\n' \
+                       'Join: @Blaster_Originals .\n' \
                        'Thank You'
     else:
         tag = message.from_user.mention
